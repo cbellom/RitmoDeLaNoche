340aedd466e6fc094ea716b51ee918c6eb388e39
unknown <ndarias@unal.edu.co>
2015-06-26 11:56:41 -0500

    added timed box page

---
+++ Ritmodelanoche/TimedBoxPage.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using Xamarin.Forms;
+
+namespace Ritmodelanoche
+{
+	public class TimedBoxPage : ContentPage
+	{
+		public TimedBoxPage ()
+		{
+			AbsoluteLayout absoluteLayout = new AbsoluteLayout();
+
+			BoxView box = new BoxView { Color = Color.Accent };
+
+			AbsoluteLayout.SetLayoutBounds(box, new Rectangle(0, 0, this.Width, this.Height));
+
+			absoluteLayout.Children.Add (box);
+			Content = absoluteLayout;
+		}
+	}
+}
+
