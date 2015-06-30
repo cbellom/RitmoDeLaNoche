1c3fbdb970be3c0a653380edc0336f46167d7120
Nicolás Arias <ndariass@unal.edu.co>
2015-06-30 12:02:15 -0500

    Keeping typography on score label update

--- Droid/ScoreLabelRenderer.cs
+++ Droid/ScoreLabelRenderer.cs
@@ -13,22 +13,47 @@ using Android.Views;
 using Android.Widget;
 using Android.OS;
 using Android.Graphics;
+using Android.Util;
 
-[assembly: ExportRenderer (typeof (ScoreLabel), typeof (ScoreLabelRenderer))]
+[assembly: ExportRenderer (typeof (Label), typeof (ScoreLabelRenderer))]
 namespace Ritmodelanoche.Droid
 {
 	public class ScoreLabelRenderer : LabelRenderer
 	{
+		private const string Tag = "ScoreLabelRenderer";
+		
 		public ScoreLabelRenderer ()
 		{
 		}
 
+		/*
 		protected override void OnElementChanged (ElementChangedEventArgs<Label> e) {
 			base.OnElementChanged (e);
-			var label = (TextView) Control; // for example
+			var label = (TextView) Control;
+
 			Typeface font = Typeface.CreateFromAsset (Forms.Context.Assets, "cs_regular.ttf");
 			label.Typeface = font;
-		}
 
+			Log.Info (Tag, ">>>Setting typography"); 
+			
+		}
+		*/
+
+		protected override void OnElementPropertyChanged(object sender, System.ComponentModel.PropertyChangedEventArgs e)
+		{
+			base.OnElementPropertyChanged(sender, e);
+
+			// check font file name
+			var typeface = Typeface.CreateFromAsset(Forms.Context.Assets, "cs_regular.ttf" );
+
+			// update font
+			var label = Control as TextView;
+			
+			if (label != null)
+				label.Typeface = typeface;
+
+			Log.Info (Tag, ">>>Setting typography"); 
+			
+		}
 	}
 }
